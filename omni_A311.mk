b4389194537c5c954923418b4c347c2c4b86195c
Tirth Patel <tirthpatel49@gmail.com>
2017-01-25 19:10:43 +0530

    Update and rename omni_minote.mk to omni_A311.mk

--- omni_A311.mk
+++ omni_A311.mk
@@ -17,6 +17,6 @@
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-$(call inherit-product, device/xiaomi/minote/full_minote.mk)
+$(call inherit-product, device/Micromax/A311/full_A311.mk)
 
-PRODUCT_NAME := omni_minote
+PRODUCT_NAME := omni_A311
